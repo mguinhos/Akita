7972862523124ddeb0f37fda8d8ae87eaf171312
mguinhos <razyrabbit@gmail.com>
2022-05-21 14:10:16 -0300

    examples: add fruits.py

---
+++ examples/fruits.py
@@ -0,0 +1,9 @@
+from stubs import *
+
+def main() -> int:
+    fruits = ["apple", "banana", "grape", "kiwi", "orange", "pear"]
+
+    print("these are fruits! yum!:\n")
+
+    for fruit in fruits:
+        print(fruit)
